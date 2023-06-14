0a2793354e0c56a7b6d91b96d0020bf4cc22de9b
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-14 13:54:38 +0530

    submitting code-Wed Jun 14 2023 13:54:28 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="shivansh" />
+    <Welcome n="shivansh" />
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,12 +2,12 @@
 import React from "react"
 
 
-const Welcome=({name})=()=>{
+const Welcome=({n})=()=>{
 
 
     return (
         <div>
-            <h1>Hey {name}!</h1>
+            <h1>Hey {n}!</h1>
             <h2>Welcome to school.</h2>
         </div>
     )
